54cd00e4a581476fff360adbba4f8cae6d63bd1b
devhoodit <72541438+devhoodit@users.noreply.github.com>
2023-08-05 12:04:33 +0900

    init

--- eclass/onlineView/onlineView.go
+++ eclass/onlineView/onlineView.go
@@ -7,7 +7,14 @@ import (
 	"io"
 	"net/http"
 	"time"
+
+	"github.com/fatih/color"
 )
+
+type ViewWorker struct {
+	Index int
+	Err   error
+}
 
 type OnlineView struct {
 	Session       string
@@ -20,30 +27,34 @@ type OnlineView struct {
 	Interval_time int
 	ReturnData    string
 	Encoding      string
+	RemainSec     int
 }
 
-func (o *OnlineView) Run(count int, ch chan int) {
+func (o *OnlineView) Run(index int, ch chan ViewWorker) {
+	green := color.New(color.BgGreen).SprintFunc()
 	err := o.checkToServer(10)
+	fmt.Printf("#%d: viewWorker => REQUEST %s | URL : https://eclass.seoultech.ac.kr/ilos/st/course/online_view_at.acl?lecture_weeks=%s&item_id=%s&link_seq=%s&his_no=%s&ky=%s&ud=%s&tm=%dtrigger_yn=%s&interval_time=%d&returnData=json&encoding=utf-8\n",
+		index, green("REQUEST SUCCESS"), o.Lecture_weeks, o.Item_id, o.Link_seq, o.His_no, o.Ky, o.Ud, 10, "N", o.Interval_time)
 	if err != nil {
-		ch <- 1
+		ch <- ViewWorker{Index: index, Err: errors.New("worker error")}
 		return
 	}
 	gap := 1
-	for i := 0; i < count; i++ {
-		fmt.Printf("next request in .. %d\n", time.Second*(time.Duration(o.Interval_time)-time.Duration(gap)))
+	count := o.RemainSec / 240
+	for i := 0; i < count+1; i++ {
 		time.Sleep(time.Second * (time.Duration(o.Interval_time) - time.Duration(gap)))
 		err := o.checkToServer(o.Interval_time - gap)
 		if err != nil {
-			ch <- 1
+			ch <- ViewWorker{Index: index, Err: errors.New("worker error")}
 			return
 		}
+		fmt.Printf("#%d: viewWorker => REQUEST %s | URL : https://eclass.seoultech.ac.kr/ilos/st/course/online_view_at.acl?lecture_weeks=%s&item_id=%s&link_seq=%s&his_no=%s&ky=%s&ud=%s&tm=%dtrigger_yn=%s&interval_time=%d&returnData=json&encoding=utf-8\n",
+			index, green("REQUEST SUCCESS"), o.Lecture_weeks, o.Item_id, o.Link_seq, o.His_no, o.Ky, o.Ud, o.Interval_time-gap, "N", o.Interval_time)
 	}
-	ch <- 0
+	ch <- ViewWorker{Index: index, Err: nil}
 }
 
 func (o *OnlineView) checkToServer(tm int) error {
-	fmt.Printf("https://eclass.seoultech.ac.kr/ilos/st/course/online_view_at.acl?lecture_weeks=%s&item_id=%s&link_seq=%s&his_no=%s&ky=%s&ud=%s&tm=%dtrigger_yn=%s&interval_time=%d&returnData=json&encoding=utf-8\n",
-		o.Lecture_weeks, o.Item_id, o.Link_seq, o.His_no, o.Ky, o.Ud, tm, "N", o.Interval_time)
 	req, err := http.NewRequest("POST", fmt.Sprintf("https://eclass.seoultech.ac.kr/ilos/st/course/online_view_at.acl?lecture_weeks=%s&item_id=%s&link_seq=%s&his_no=%s&ky=%s&ud=%s&tm=%dtrigger_yn=%s&interval_time=%d&returnData=json&encoding=utf-8",
 		o.Lecture_weeks, o.Item_id, o.Link_seq, o.His_no, o.Ky, o.Ud, tm, "N", o.Interval_time), nil)
 	if err != nil {
